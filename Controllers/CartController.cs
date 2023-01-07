4b87d6b09d4bd03b5662e9029a02d91cefebf71a
Keith Pham <minhnghia2208@gmail.com>
2023-01-07 16:33:11 -0500

    Add project files.

---
+++ Controllers/CartController.cs
@@ -0,0 +1,91 @@
+﻿using Dapr.Client;
+using Google.Type;
+using Azure.Messaging.ServiceBus;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ShoppingCartService.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ShoppingCartService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        // GET: api/<CartController>
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            //var daprClient = new DaprClientBuilder().Build();
+
+            //await daprClient.PublishEventAsync<string>("pubsub", "newOrder", "Testing");
+            ServiceBusClient client;
+            ServiceBusSender sender;
+
+            var clientOptions = new ServiceBusClientOptions()
+            {
+                TransportType = ServiceBusTransportType.AmqpWebSockets
+            };
+            client = new ServiceBusClient("Endpoint=sb://daprentity.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=iGY15mccd2kyP58QI+c/2y+ttvUS8vS4TVWdokZQOIc=", clientOptions);
+            sender = client.CreateSender("order");
+
+            // create a batch 
+            using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                // try adding a message to the batch
+                Order newOrder = new Order() { Id = i,  Amount = 1, Name = "Testing" };
+                string messageBody = JsonConvert.SerializeObject(newOrder);
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage(messageBody)))
+                {
+                    // if it is too large for the batch
+                    throw new Exception($"The message {i} is too large to fit in the batch.");
+                }
+            }
+
+            try
+            {
+                // Use the producer client to send the batch of messages to the Service Bus queue
+                await sender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of 5 messages has been published to the queue.");
+            }
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CartController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CartController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+     
+        }
+
+        // PUT api/<CartController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CartController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Model/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingCartService.Model
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Amount { get; set; }
+    }
+}
